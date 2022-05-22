3fcc62093bf3c008d17bfa6ce8bd1ddddeda02e6
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2022-05-22 12:37:50 +0530

    Made a route
    In App.js we can see in new version how we have to make a route.

--- amazonclone/src/App.js
+++ amazonclone/src/App.js
@@ -1,9 +1,17 @@
+import React from 'react';
 import './App.css';
+import Home from './components/Home';
+import { BrowserRouter, BrowserRouter as Router, Routes,Route } from "react-router-dom";
 
 function App() {
   return (
     <div className='app'>
       Hello world
+      <Router>
+          <Routes>
+            <Route path='/we' element={<Home/>}/>
+          </Routes>
+      </Router>
     </div>
   );
 }
---
+++ amazonclone/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div className="home">
+            Hello world again!!
+        </div>
+    )
+}
+
+export default Home
