3c334df5095df1839556b4e4cd83a4896a07e359
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2022-05-25 12:10:50 +0530

    Set up the homepage
    We have shown products using the product info and make using css and js

--- amazonclone/src/App.js
+++ amazonclone/src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 import Home from './components/Home';
 import Header from './components/Header';
+import Product from './components/Product';
 import { BrowserRouter, BrowserRouter as Router, Routes,Route } from "react-router-dom";
 
 function App() {
   return (
     <div className='app'>
+      {/* <Home/> */}
       <Router>
           <Routes>
-            <Route path='/' element={((<Home/>),(<Header/>))}/>
+            <Route path='/'
+              element={
+                <Fragment>
+                    <Header/>
+                    <Home/>
+                    <Product/>
+                </Fragment>
+              }
+            />
           </Routes>
       </Router>
     </div>
